5aabb0120ce99a2da126139797f050d4f9e6f78b
Jude Quintana <jude.quintana@jumpcloud.com>
2021-08-11 15:59:12 -0600

    intra vpc security group rule module

---
+++ networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/main.tf
@@ -0,0 +1,47 @@
+locals {
+  # Each VPC id should have an inbound rule from all other VPC networks except itself
+  # The next 3 for loops will build the following structure
+  # given vpc1 vpc2 vpc3
+  # vpc1_id => [vpc2_network, vpc3_network]
+  # vpc2_id => [vpc1_network, vpc3_network]
+  # vpc3_id => [vpc1_network, vpc2_network]
+  # so on and so forth
+  # use as a lookup table for local.vpc_to_intra_vpc_security_groups
+  vpc_id_to_networks = { for vpc_name, this in var.vpcs : this.id => this.network }
+
+  vpc_ids_vpc_networks_product = setproduct(
+    keys(local.vpc_id_to_networks),
+    values(local.vpc_id_to_networks)
+  )
+
+  vpc_id_to_inbound_networks = {
+    for vpc_id_network in local.vpc_ids_vpc_networks_product :
+    vpc_id_network[0] => vpc_id_network[1]...
+    if lookup(local.vpc_id_to_networks, vpc_id_network[0]) != vpc_id_network[1]
+  }
+
+  # build a security group rule object for each vpc with vpc_name for the key
+  vpc_to_intra_vpc_security_group_rules = {
+    for vpc_name, this in var.vpcs :
+    vpc_name => merge({
+      intra_vpc_security_group_id = this.intra_vpc_security_group_id
+      networks                    = lookup(local.vpc_id_to_inbound_networks, this.id)
+    }, var.rule)
+  }
+}
+
+resource "aws_security_group_rule" "this" {
+  for_each = local.vpc_to_intra_vpc_security_group_rules
+
+  security_group_id = each.value.intra_vpc_security_group_id
+  cidr_blocks       = each.value.networks
+  type              = "ingress"
+  from_port         = each.value.from_port
+  to_port           = each.value.to_port
+  protocol          = each.value.protocol
+  description = format(
+    "%s Env: Allow %s inbound across VPCs",
+    upper(var.env_prefix),
+    each.value.label
+  )
+}
---
+++ networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/variables.tf
@@ -0,0 +1,28 @@
+variable "env_prefix" {
+  description = "prod, stage, test"
+  type        = string
+}
+
+variable "rule" {
+  description = "security rule object to allow inbound across vpcs intra-vpc security group"
+  type = object({
+    label     = string
+    from_port = number
+    to_port   = number
+    protocol  = string
+  })
+}
+
+variable "vpcs" {
+  description = "map of tiered_vpc_ng objects"
+  type = map(object({
+    id                          = string
+    network                     = string
+    intra_vpc_security_group_id = string
+  }))
+
+  validation {
+    condition     = length(var.vpcs) > 1
+    error_message = "There must be at least 2 VPCs."
+  }
+}
