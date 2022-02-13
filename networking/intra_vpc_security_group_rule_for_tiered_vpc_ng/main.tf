b124cb19afa98f09fa42cb2820a7a9f1d1fd66ae
Jude Quintana <jude.quintana@gmail.com>
2022-02-13 16:55:06 -0700

    try this

--- networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/main.tf
+++ networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/main.tf
@@ -7,10 +7,14 @@ locals {
   #   ...
   # }
   # local.vpc_id_to_inbound_networks will then be used as a lookup table for local.vpc_to_intra_vpc_security_group_rules
+  #
+  #
+  vpc_id_to_networks = { for vpc_name, this in var.vpcs : this.id => this.network }
+
   vpc_id_to_inbound_networks = {
     for vpc_id_and_network in setproduct(var.vpcs[*].id, var.vpcs[*].network) :
     vpc_id_and_network[0] => vpc_id_and_network[1]...
-    if lookup(local.vpc_id_and_networks, vpc_id_and_network[0]) != vpc_id_and_network[1]
+    if lookup(local.vpc_id_to_networks, vpc_id_and_network[0]) != vpc_id_and_network[1]
   }
 
   # build a security group rule object for each vpc with vpc_name for the key
