f6c20ffc2adc6877e958f7e48ace213e25c3ea9a
Jude Quintana <jude.quintana@gmail.com>
2022-02-13 17:09:10 -0700

    updates

--- networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/main.tf
+++ networking/intra_vpc_security_group_rule_for_tiered_vpc_ng/main.tf
@@ -6,11 +6,11 @@ locals {
   #   vpc3-id = [vpc1-network, vpc2-network, ...]
   #   ...
   # }
-  # local.vpc_id_to_inbound_networks will then be used as a lookup table for local.vpc_to_intra_vpc_security_group_rules
-  #
-  #
+
+  # local.vpc_id_to_networks is used as a lookup table for local.vpc_id_to_inbound_networks 'if' logic
   vpc_id_to_networks = { for vpc_name, this in var.vpcs : this.id => this.network }
 
+  # local.vpc_id_to_inbound_networks will then be used as a lookup table for local.vpc_to_intra_vpc_security_group_rules
   vpc_id_to_inbound_networks = {
     for vpc_id_and_network in setproduct(keys(local.vpc_id_to_networks), values(local.vpc_id_to_networks)) :
     vpc_id_and_network[0] => vpc_id_and_network[1]...
