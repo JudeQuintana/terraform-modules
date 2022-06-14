d7295df4ad9993423a5b59f59914bc09299b3a74
Jude Quintana <jude.quintana@gmail.com>
2022-06-14 09:16:35 -0600

    updates

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/main.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/main.tf
@@ -40,19 +40,21 @@ locals {
   # i'm not sure about security implications of this pattern but i dont think it matters.
   #
   # { vpc-1-id  = [ "first-public-subnet-id-of-az-1-for-vpc-1", "first-public-subnet-id-of-az-2-for-vpc-1", ... ], ...}
+  vpc_id_to_single_public_subnet_ids_per_az = {}
   #vpc_id_to_single_public_subnet_ids_per_az = {
   #for vpc_name, this in var.vpcs :
   #this.id => [for az, public_subnet_ids in this.az_to_public_subnet_ids : element(public_subnet_ids, 0)]
   #}
 
   # lookup table for each aws_ec2_transit_gateway_vpc_attachment to get the name based on id
+  vpc_id_to_names = {}
   #vpc_id_to_names = { for vpc_name, this in var.vpcs : this.id => vpc_name }
 }
 
 # attach vpcs to tgw
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  #for_each = local.vpc_id_to_single_public_subnet_ids_per_az
-  for_each = {}
+  for_each = local.vpc_id_to_single_public_subnet_ids_per_az
+  #for_each = {}
 
   subnet_ids                                      = each.value
   transit_gateway_id                              = aws_ec2_transit_gateway.this.id
@@ -78,8 +80,8 @@ resource "aws_ec2_transit_gateway_route_table" "this" {
 
 ## associate attachments to route table
 resource "aws_ec2_transit_gateway_route_table_association" "this" {
-  #for_each = local.vpc_id_to_single_public_subnet_ids_per_az
-  for_each = {}
+  for_each = local.vpc_id_to_single_public_subnet_ids_per_az
+  #for_each = {}
 
   transit_gateway_attachment_id  = lookup(aws_ec2_transit_gateway_vpc_attachment.this, each.key).id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
@@ -87,40 +89,41 @@ resource "aws_ec2_transit_gateway_route_table_association" "this" {
 
 # route table propagation
 resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
-  #for_each = local.vpc_id_to_single_public_subnet_ids_per_az
-  for_each = {}
+  for_each = local.vpc_id_to_single_public_subnet_ids_per_az
+  #for_each = {}
 
   transit_gateway_attachment_id  = lookup(aws_ec2_transit_gateway_vpc_attachment.this, each.key).id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
 }
 
 # Create routes to other VPC networks in private and public route tables for each VPC
-#locals {
-## { vpc-1-network => [ "vpc-1-private-rtb-id-1", "vpc-1-public-rtb-id-1", ... ], ...}
-#vpc_network_to_private_and_public_route_table_ids = {
-#for vpc_name, this in var.vpcs :
-#this.network => concat(values(this.az_to_private_route_table_id), values(this.az_to_public_route_table_id))
-#}
+locals {
+  # { vpc-1-network => [ "vpc-1-private-rtb-id-1", "vpc-1-public-rtb-id-1", ... ], ...}
+  vpc_network_to_private_and_public_route_table_ids = {}
+  #vpc_network_to_private_and_public_route_table_ids = {
+  #for vpc_name, this in var.vpcs :
+  #this.network => concat(values(this.az_to_private_route_table_id), values(this.az_to_public_route_table_id))
+  #}
 
-## [ { rtb_id = "vpc-1-rtb-id-123", other_networks = [ "other-vpc-2-network", "other-vpc3-network", ... ] }, ...]
-#associate_private_and_public_route_table_ids_with_other_networks = flatten(
-#[for network, rtb_ids in local.vpc_network_to_private_and_public_route_table_ids :
-#[for rtb_id in rtb_ids : {
-#rtb_id         = rtb_id
-#other_networks = [for n in keys(local.vpc_network_to_private_and_public_route_table_ids) : n if n != network]
-#}]])
+  # [ { rtb_id = "vpc-1-rtb-id-123", other_networks = [ "other-vpc-2-network", "other-vpc3-network", ... ] }, ...]
+  associate_private_and_public_route_table_ids_with_other_networks = flatten(
+    [for network, rtb_ids in local.vpc_network_to_private_and_public_route_table_ids :
+      [for rtb_id in rtb_ids : {
+        rtb_id         = rtb_id
+        other_networks = [for n in keys(local.vpc_network_to_private_and_public_route_table_ids) : n if n != network]
+  }]])
 
-## { rtb-id|route => route, ... }
-#private_and_public_routes_to_other_networks = merge(
-#[for r in local.associate_private_and_public_route_table_ids_with_other_networks :
-#{ for rtb_id_and_route in setproduct([r.rtb_id], r.other_networks) :
-#format("%s|%s", rtb_id_and_route[0], rtb_id_and_route[1]) => rtb_id_and_route[1] # each key must be unique, dont group by key
-#}]...)
-#}
+  # { rtb-id|route => route, ... }
+  private_and_public_routes_to_other_networks = merge(
+    [for r in local.associate_private_and_public_route_table_ids_with_other_networks :
+      { for rtb_id_and_route in setproduct([r.rtb_id], r.other_networks) :
+        format("%s|%s", rtb_id_and_route[0], rtb_id_and_route[1]) => rtb_id_and_route[1] # each key must be unique, dont group by key
+  }]...)
+}
 
 resource "aws_route" "this" {
-  #for_each = local.private_and_public_routes_to_other_networks
-  for_each = {}
+  for_each = local.private_and_public_routes_to_other_networks
+  #for_each = {}
 
   destination_cidr_block = each.value
   route_table_id         = split("|", each.key)[0]
