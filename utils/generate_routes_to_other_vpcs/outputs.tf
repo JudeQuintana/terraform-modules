3a4c407fb3b37556d8e769d65f09d1b347cfafe7
Jude Quintana <jude.quintana@gmail.com>
2022-06-25 13:51:35 -0600

    output list of objects too

--- utils/generate_routes_to_other_vpcs/outputs.tf
+++ utils/generate_routes_to_other_vpcs/outputs.tf
@@ -2,3 +2,8 @@
 output "call" {
   value = local.private_and_public_routes_to_other_networks
 }
+# output routes as list of objects instead of a map
+# { rtb_id = "rtb-1234", route = "x.x.x.x/x" }
+output "call_routes" {
+  value = local.routes
+}
