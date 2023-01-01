af923b5efd990366da15ca75ffbf3fed87a19e44
Jude Quintana <jude.quintana@gmail.com>
2023-01-01 16:41:03 -0700

    clean up

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/variables.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/variables.tf
@@ -1,19 +1,3 @@
-#variable "amazon_side_asn" {
-#description = "required local amazon side asn"
-#type        = number
-#}
-
-#variable "blackhole_subnets" {
-#description = "subnets to blackhole."
-#type        = list(string)
-#default     = []
-#}
-
-#variable "name" {
-#description = "centralized router name"
-#type        = string
-#}
-
 variable "env_prefix" {
   description = "prod, stage, test"
   type        = string
@@ -23,34 +7,6 @@ variable "region_az_labels" {
   description = "Region and AZ names mapped to short naming conventions for labeling"
   type        = map(string)
 }
-
-variable "tags" {
-  description = "Additional Tags"
-  type        = map(string)
-  default     = {}
-}
-
-#variable "vpcs" {
-#description = "map of tiered_vpc_ng objects"
-#type = map(object({
-#account_id                   = string
-#az_to_private_route_table_id = map(string)
-#az_to_private_subnet_ids     = map(list(string))
-#az_to_public_route_table_id  = map(string)
-#az_to_public_subnet_ids      = map(list(string))
-#region                       = string
-#id                           = string
-#full_name                    = string
-#network                      = string
-#}))
-#default = {}
-
-#validation {
-#condition     = length(distinct([for this in var.vpcs : this.network])) == length([for this in var.vpcs : this.network])
-#error_message = "All VPCs must have unique network CIDRs."
-#}
-#}
-
 
 variable "centralized_router" {
   description = "centralized router configuration"
@@ -76,3 +32,10 @@ variable "centralized_router" {
     error_message = "All VPCs must have unique network CIDRs."
   }
 }
+
+variable "tags" {
+  description = "Additional Tags"
+  type        = map(string)
+  default     = {}
+}
+
