4b5da62cd45dd78716a8be16c7929dd7f3316713
Jude Quintana <jude.quintana@gmail.com>
2023-01-02 11:57:11 -0700

    update outputs

--- networking/tgw_super_router_for_tgw_centralized_router/outputs.tf
+++ networking/tgw_super_router_for_tgw_centralized_router/outputs.tf
@@ -1,21 +1,31 @@
+output "name" {
+  value = var.super_router.name
+}
+
+output "blackhole_subnets" {
+  value = var.super_router.blackhole_subnets
+}
+
 output "local" {
   value = {
-    account_id     = local.local_account_id
-    full_name      = local.local_super_router_name
-    id             = aws_ec2_transit_gateway.this_local.id
-    networks       = local.local_tgws_all_vpc_networks
-    region         = local.local_region_name
-    route_table_id = aws_ec2_transit_gateway_route_table.this_local.id
+    account_id      = local.local_account_id
+    amazon_side_asn = var.super_router.local.amazon_side_asn
+    full_name       = local.local_super_router_name
+    id              = aws_ec2_transit_gateway.this_local.id
+    networks        = local.local_tgws_all_vpc_networks
+    region          = local.local_region_name
+    route_table_id  = aws_ec2_transit_gateway_route_table.this_local.id
   }
 }
 
 output "peer" {
   value = {
-    account_id     = local.peer_account_id
-    full_name      = local.peer_super_router_name
-    id             = aws_ec2_transit_gateway.this_peer.id
-    networks       = local.peer_tgws_all_vpc_networks
-    region         = local.peer_region_name
-    route_table_id = aws_ec2_transit_gateway_route_table.this_peer.id
+    account_id      = local.peer_account_id
+    amazon_side_asn = var.super_router.peer.amazon_side_asn
+    full_name       = local.peer_super_router_name
+    id              = aws_ec2_transit_gateway.this_peer.id
+    networks        = local.peer_tgws_all_vpc_networks
+    region          = local.peer_region_name
+    route_table_id  = aws_ec2_transit_gateway_route_table.this_peer.id
   }
 }
