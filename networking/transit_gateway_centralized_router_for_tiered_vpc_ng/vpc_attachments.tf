53e72c5c8648a65fe61162c471a4bc043bb67e3d
Jude Quintana <jude.quintana@gmail.com>
2023-01-04 11:31:29 -0700

    actual fix

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/vpc_attachments.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/vpc_attachments.tf
@@ -10,13 +10,19 @@ locals {
     #this.id => [for subnet_id in concat(values(this.az_to_private_subnet_ids), values(this.az_to_public_subnet_ids)) : subnet_id]
   }
 
+  vpc_id_to_single_subnet_ids_per_az = {
+    for vpc_id, this in local.vpc_id_to_single_private_and_public_subnet_ids_per_az :
+    vpc_id => [element(this, 0)]
+  }
+
+
   # lookup table for each aws_ec2_transit_gateway_vpc_attachment to get the name based on id
   vpc_id_to_names = { for vpc_name, this in var.centralized_router.vpcs : this.id => this.full_name }
 }
 
 # attach vpcs to tgw
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
-  for_each = local.vpc_id_to_single_private_and_public_subnet_ids_per_az
+  for_each = local.vpc_id_to_single_subnet_ids_per_az
 
   subnet_ids                                      = each.value
   transit_gateway_id                              = aws_ec2_transit_gateway.this.id
@@ -31,7 +37,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
 
 # associate attachments to route table
 resource "aws_ec2_transit_gateway_route_table_association" "this" {
-  for_each = local.vpc_id_to_single_private_and_public_subnet_ids_per_az
+  for_each = local.vpc_id_to_single_subnet_ids_per_az
 
   transit_gateway_attachment_id  = lookup(aws_ec2_transit_gateway_vpc_attachment.this, each.key).id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
@@ -39,7 +45,7 @@ resource "aws_ec2_transit_gateway_route_table_association" "this" {
 
 # route table propagation
 resource "aws_ec2_transit_gateway_route_table_propagation" "this" {
-  for_each = local.vpc_id_to_single_private_and_public_subnet_ids_per_az
+  for_each = local.vpc_id_to_single_subnet_ids_per_az
 
   transit_gateway_attachment_id  = lookup(aws_ec2_transit_gateway_vpc_attachment.this, each.key).id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
