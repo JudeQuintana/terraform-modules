ff6de8cbf6bd6d56f7bd2a9e6015062b63eb354f
Jude Quintana <jude.quintana@gmail.com>
2023-01-15 13:10:21 -0700

    clarity

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/outputs.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/outputs.tf
@@ -36,10 +36,10 @@ output "vpc_network_cidrs" {
 
 # route object will only have 3 attributes instead of all attributes from the route
 # makes it easier to see when troubleshooting many vpc routes
-# otherwise it can just be [for this in aws_route.this : this]
+# otherwise it can just be [for this in aws_route.this_vpc_routes_to_other_vpcs : this]
 output "vpc_routes" {
   value = [
-    for this in aws_route.this : {
+    for this in aws_route.this_vpc_routes_to_other_vpcs : {
       route_table_id         = this.route_table_id
       destination_cidr_block = this.destination_cidr_block
       transit_gateway_id     = this.transit_gateway_id
