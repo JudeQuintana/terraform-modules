bb8e3f86c4570dfe4a658a19458cdfa3fda83dad
Jude Quintana <jude.quintana@gmail.com>
2023-01-15 19:16:27 -0700

    update

--- networking/super_intra_vpc_access/variables.tf
+++ networking/super_intra_vpc_access/variables.tf
@@ -10,5 +10,37 @@ variable "region_az_labels" {
 
 variable "super_intra_vpc_access" {
   description = "super intra vpc access configuration"
-  type        = object({})
+  type = object({
+    # security rule object to allow inbound across vpcs intra-vpc security group
+    local = object({
+      rule = object({
+        label     = string
+        protocol  = string
+        from_port = number
+        to_port   = number
+      })
+      # map of tiered_vpc_ng objects
+      vpcs = map(object({
+        id                          = string
+        intra_vpc_security_group_id = string
+        network_cidr                = string
+        region                      = string
+      }))
+    })
+    peer = object({
+      rule = object({
+        label     = string
+        protocol  = string
+        from_port = number
+        to_port   = number
+      })
+      # map of tiered_vpc_ng objects
+      vpcs = map(object({
+        id                          = string
+        intra_vpc_security_group_id = string
+        network_cidr                = string
+        region                      = string
+      }))
+    })
+  })
 }
