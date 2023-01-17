fa4a080e25051348ee3cdada8682b3ed65678fe1
Jude Quintana <jude.quintana@gmail.com>
2023-01-16 22:23:56 -0700

    centralized router docs

---
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/main.tf
@@ -0,0 +1,31 @@
+/*
+* # Networking Trifecta Demo
+* Blog Post:
+* [Terraform Networking Trifecta ](https://jq1.io/posts/tnt/)
+*
+* Main:
+* - [Networking Trifecta Demo](https://github.com/JudeQuintana/terraform-main/tree/main/networking_trifecta_demo)
+  * - See [Trifecta Demo Time](https://jq1.io/posts/tnt/#trifecta-demo-time) for instructions.
+*
+* # Transit Gateway Centralized Router Description
+* - This Transit Gateway Centralized Router module will use the first public subnet in the list for each AZ of that VPC for the VPC attachments to all AZs.
+* - All attachments will be associated and routes propagated to one TGW Route Table.
+* - Each Tiered VPC will have all their route tables updated in each VPC with a route to all other VPC networks via the TGW.
+* - Will generate and add routes in each VPC to all other networks.
+*
+* Example:
+* ```
+* module "centralized_router" {
+*   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/transit_gateway_centralized_router_for_tiered_vpc_ng?ref=moar-better"
+*
+*   env_prefix       = var.env_prefix
+*   region_az_labels = var.region_az_labels
+*   centralized_router = {
+*     name            = "bishop"
+*     amazon_side_asn = 64512
+*     blackhole_cidrs = ["172.16.8.0/24"]
+*     vpcs            = module.vpcs
+*   }
+* }
+* ```
+*/
