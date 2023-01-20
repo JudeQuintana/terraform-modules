274cc2cf1624d36fdaa5409fa10ca8d796113ec4
Jude Quintana <jude.quintana@gmail.com>
2023-01-20 10:15:49 -0700

    naming for super router peering attachments

--- networking/tgw_super_router_for_tgw_centralized_router/peering.tf
+++ networking/tgw_super_router_for_tgw_centralized_router/peering.tf
@@ -23,7 +23,7 @@ resource "aws_ec2_transit_gateway_peering_attachment" "this_local_to_this_peer" 
   peer_transit_gateway_id = aws_ec2_transit_gateway.this_peer.id
   transit_gateway_id      = aws_ec2_transit_gateway.this_local.id
   tags = {
-    Name = local.local_super_router_name
+    Name = format(local.peering_name_format, local.local_super_router_name, local.peer_super_router_name)
     Side = "Local Creator"
   }
 }
@@ -35,6 +35,7 @@ resource "aws_ec2_transit_gateway_peering_attachment_accepter" "this_local_to_th
   transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.this_local_to_this_peer.id
   tags = {
     Name = local.peer_super_router_name
+    Name = format(local.peering_name_format, local.peer_super_router_name, local.local_super_router_name)
     Side = "Peer Accepter"
   }
 }
