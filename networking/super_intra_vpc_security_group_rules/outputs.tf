d262cfb27dab04ec1a56036144c1aacbef6684f3
Jude Quintana <jude.quintana@gmail.com>
2023-01-27 00:07:57 -0700

    fix

--- networking/super_intra_vpc_security_group_rules/outputs.tf
+++ networking/super_intra_vpc_security_group_rules/outputs.tf
@@ -2,7 +2,7 @@ output "local" {
   value = {
     account_id = local.local_account_id
     region     = local.local_region_name
-    rules      = [for this in local.local_vpc_id_to_peer_intra_vpc_security_group_rules : this]
+    rules      = [for this in local.local_vpc_id_and_rule_to_peer_intra_vpc_security_group_rule : this]
   }
 }
 
@@ -10,7 +10,7 @@ output "peer" {
   value = {
     account_id = local.peer_account_id
     region     = local.peer_region_name
-    rules      = [for this in local.peer_vpc_id_to_local_intra_vpc_security_group_rules : this]
+    rules      = [for this in local.peer_vpc_id_and_rule_to_local_intra_vpc_security_group_rule : this]
   }
 }
 
