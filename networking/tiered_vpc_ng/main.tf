476021b2d912adfd76ef881a426eebc324a22b9e
Jude Quintana <jude.quintana@gmail.com>
2023-01-29 21:52:50 -0700

    docs

--- networking/tiered_vpc_ng/main.tf
+++ networking/tiered_vpc_ng/main.tf
@@ -14,14 +14,17 @@
 * - An Intra VPC Security Group is created by default.
 *  - This will be for adding security group rules that are inbound only for access across VPCs.
 *
-* - Can add, remove, name and rename sunbnets*.
-*
-* - Requires a minimum of at least one public subnet per AZ.
-*   - When a NAT Gateway is enabled for an AZ it will be built in the public subnet with the `special = true` attribute for an AZ by default. There can only be one public subnet with `special = true` per AZ.
-*     - Set `enable_natgw` in an AZ to give all private subnets in an AZ access to the internet, in which case the NAT Gateway will be built with EIP allocated and private route tables updated.
-*   - When a Tiered VPC is passed to a Centralized Router, the VPC attachment will also use the public subnet with the `special = true` attribute for each AZ of that VPC by default.
-*   - The trade off is always having to allocate at least one public subnet per AZ, even if the you don't need to use it (ie using private subnets only).
-*   - I highly recommend naming and allocating a small subnet like a /28 and setting it's special attribute to true (ie `public_subnets = [{name = "natwgw", cidr = "10.0.9.64/28", special = true}, {name = "haproxy1", cidr = "10.0.10.0/24"}]`.
+* - Can access subnet id by subnet name map via output.
+* - Can rename public and private subnets directly without forcing a new subnet resource.
+* - Public subnets now have a special attribute option.
+*   - Only one can have `special = true` which enables 3 things:
+*   - Associate a NAT Gateway if `enable_natgw = true`.
+    - Use for associating VPC attatchments when Tiered VPC is passed to a Centralized Router (one in each AZ).
+*   - Existing public subnets can be rearranged in any order in their repective subnet list without forcing new resources.
+*   - The trade off is always having to allocate one public subnet per AZ, even if you don’t need to use it (ie using private subnets only).
+* - Important:
+*   - All VPC names should be unique across regions (validation enforced).
+*   - I highly recommend allocating a small public subnet like a /28 and setting it’s special attribute to true for each AZ.
 *   - Subnets can only be deleted when they are not in use (ie natgws, vpc attachment, ec2 instances, or some other aws server).
 *
 * - Recommendations:
