85bc4aa70e9bc252e55aae6b3eedac1eeb315d90
Jude Quintana <jude.quintana@gmail.com>
2023-02-26 15:57:28 -0700

    update

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/base.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/base.tf
@@ -17,7 +17,6 @@ locals {
   }, var.tags)
 
   centralized_router_name = format("%s-%s-%s-%s", local.upper_env_prefix, "centralized-router", var.centralized_router.name, local.region_label)
-
   # easier to use list of objects internally
   vpcs = [for this in var.centralized_router.vpcs : this]
 }
