d4d9a659fe9e4f5223c4e5acf515ec228eaf90e9
Jude Quintana <jude.quintana@gmail.com>
2023-06-25 13:25:24 -0600

    fix link

--- networking/ultra_cloudwan/main.tf
+++ networking/ultra_cloudwan/main.tf
@@ -6,7 +6,7 @@
 * ![super-router-base-architecture](https://jq1-io.s3.amazonaws.com/super-router/super-router-base-architecture.png)
 *
 * I was was thinking about building a cloudwan module to connect several super routers togther but only go thru cloudwan if the destination region is not one of the super router’s tgw pair (ie. us-west-2 <=> eu-central-1):
-* ![ultra-cloudwan-init](https://jq1-io.s3.amazonaws.com/super-router/ultra-cloudwan-init.png)
+* ![ultra-cloudwan-init](https://jq1-io.s3.amazonaws.com/ultra-cloudwan/ultra-cloudwan-init.png)
 *
 *
 * */
