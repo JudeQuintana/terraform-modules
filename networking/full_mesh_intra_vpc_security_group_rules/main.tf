a6fbeef3cb1eb6084c6209749066698bc4ee1a12
Jude Quintana <jude.quintana@gmail.com>
2023-12-03 12:22:47 -0700

    fix links

--- networking/full_mesh_intra_vpc_security_group_rules/main.tf
+++ networking/full_mesh_intra_vpc_security_group_rules/main.tf
@@ -1,9 +1,9 @@
 /*
 * # Full Mesh Intra VPC Secuity Group Rules Description
 * - This allowing inbound protocols across regions based on rules (ie ssh, icmp, etc) that
-*   were used in each intra\_vpc\_security\_group\_rules modules for all vpcs in each region.
+*   were used in each intra_vpc_security_group_rules modules for all vpcs in each region while in a TGW full mesh configuration.
 * - Rule sets for one, two and three Intra VPC Security Group Rules should be the same. also enforced by validation
-* - See it in action in [security\_group\_rules.tf](https://github.com/JudeQuintana/terraform-main/blob/full-mesh-intra-vpc-security-rules/full_mesh_trio_demo/security_group_rules.tf) in the [Full Mesh Trio Demo](https://github.com/JudeQuintana/terraform-main/tree/full-mesh-intra-vpc-security-rules/full_mesh_trio_demo).
+* - See it in action in [security_group_rules.tf](https://github.com/JudeQuintana/terraform-main/blob/main/full_mesh_trio_demo/security_group_rules.tf) in the [Full Mesh Trio Demo](https://github.com/JudeQuintana/terraform-main/tree/main/full_mesh_trio_demo).
 *
 * ```
 * module "full_mesh_intra_vpc_security_groups_rules" {
