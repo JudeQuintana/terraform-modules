060784d38272d93db9d39d261180bf1a1c4034f0
Jude Quintana <jude.quintana@gmail.com>
2023-12-10 10:04:55 -0700

    reorg

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/base.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/base.tf
@@ -5,14 +5,15 @@ data "aws_caller_identity" "this" {}
 data "aws_region" "this" {}
 
 locals {
-  account_id              = data.aws_caller_identity.this.account_id
-  region_name             = data.aws_region.this.name
-  region_label            = lookup(var.region_az_labels, local.region_name)
-  upper_env_prefix        = upper(var.env_prefix)
-  centralized_router_name = format("%s-%s-%s-%s", local.upper_env_prefix, "centralized-router", var.centralized_router.name, local.region_label)
+  account_id       = data.aws_caller_identity.this.account_id
+  region_name      = data.aws_region.this.name
+  region_label     = lookup(var.region_az_labels, local.region_name)
+  upper_env_prefix = upper(var.env_prefix)
   default_tags = merge({
     Environment = var.env_prefix
   }, var.tags)
+
+  centralized_router_name = format("%s-%s-%s-%s", local.upper_env_prefix, "centralized-router", var.centralized_router.name, local.region_label)
 }
 
 # one tgw that will route between all tiered vpcs.
@@ -37,14 +38,5 @@ resource "aws_ec2_transit_gateway" "this" {
       error_message = "All VPC account IDs must match the aws provider account ID for Centralized Router."
     }
   }
-}
-
-# one route table for all vpc network_cidrs
-resource "aws_ec2_transit_gateway_route_table" "this" {
-  transit_gateway_id = aws_ec2_transit_gateway.this.id
-  tags = merge(
-    local.default_tags,
-    { Name = local.centralized_router_name }
-  )
 }
 
