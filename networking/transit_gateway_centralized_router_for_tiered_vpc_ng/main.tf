a393ecf5f41d83d9e0aa3f621007fa471f83af46
Jude Quintana <jude.quintana@gmail.com>
2024-05-22 20:12:49 -0600

    docs

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/main.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/main.tf
@@ -1,12 +1,19 @@
 /*
 * # Transit Gateway Centralized Router Description
-* - This Transit Gateway Centralized Router module will create a hub spoke topology from existing Tiered VPCs.
+* `v1.0.1`
+* - Now supports VPC attachments for private subnets.
+*   - Will use the subnet id for a subnet tagged with `special = true` from either a private or a public subnet per AZ in Tiered VPC-NG for `v1.0.1`
+* - Not ideal to migrate to `v1.0.0` since there are many resource naming changes.
+*   - Recommend starting fresh at `v1.0.1`
+*
+* `v1.0.0`
+* - This Transit Gateway Centralized Router module will create a hub spoke and topology from existing Tiered VPCs.
 * - Will use the special public subnet in each AZ when a Tiered VPC is passed to it.
 * - All attachments will be associated and routes propagated to one TGW Route Table.
 * - Each Tiered VPC will have all their route tables updated in each VPC with a route to all other VPC networks via the TGW.
 * - Will generate and add routes in each VPC to all other networks.
 *
-* Example:
+* `v1.0.0` Example:
 * ```
 * module "centralized_router" {
 *   source = "git@github.com:JudeQuintana/terraform-modules.git//networking/transit_gateway_centralized_router_for_tiered_vpc_ng?ref=v1.7.5"
