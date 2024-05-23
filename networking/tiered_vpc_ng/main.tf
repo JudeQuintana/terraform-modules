71b7f60bf68824c590c4e07eef42494f3e2718b4
Jude Quintana <jude.quintana@gmail.com>
2024-05-22 20:15:55 -0600

    docs

--- networking/tiered_vpc_ng/main.tf
+++ networking/tiered_vpc_ng/main.tf
@@ -12,6 +12,69 @@
 * - Not ideal to migrate to `v1.0.0` since there are many resource naming changes.
 *   - Recommend starting fresh at `v1.0.1`
 *
+* `v1.0.1` example:
+* ```
+* locals {
+*   tiered_vpcs = [
+*     {
+*       name         = "app"
+*       network_cidr = "10.0.0.0/20"
+*       azs = {
+*         a = {
+*           public_subnets = [
+*             { name = "random1", cidr = "10.0.3.0/28" },
+*             { name = "haproxy1", cidr = "10.0.4.0/26" },
+*             { name = "other", cidr = "10.0.10.0/28", special = true }
+*           ]
+*         }
+*         b = {
+*           private_subnets = [
+*             { name = "cluster2", cidr = "10.0.1.0/24" },
+*             { name = "random2", cidr = "10.0.5.0/24", special = true }
+*           ]
+*         }
+*       }
+*     },
+*     {
+*       name         = "cicd"
+*       network_cidr = "172.16.0.0/20"
+*       azs = {
+*         b = {
+*           private_subnets = [
+*             { name = "jenkins1", cidr = "172.16.5.0/24" }
+*           ]
+*           public_subnets = [
+*             { name = "natgw", cidr = "172.16.8.0/28", special = true, natgw = true },
+*           ]
+*         }
+*       }
+*     },
+*     {
+*       name         = "general"
+*       network_cidr = "192.168.0.0/20"
+*       azs = {
+*         c = {
+*           private_subnets = [
+*             { name = "db1", cidr = "192.168.10.0/24", special = true }
+*           ]
+*         }
+*       }
+*     }
+*   ]
+* }
+*
+* module "vpcs" {
+*   source  = "JudeQuintana/tiered-vpc-ng/aws"
+*   version = "1.0.1"
+*
+*   for_each = { for t in local.tiered_vpcs : t.name => t }
+*
+*   env_prefix       = var.env_prefix
+*   region_az_labels = var.region_az_labels
+*   tiered_vpc       = each.value
+* }
+* ```
+*
 * `v1.0.0`
 * - Create VPC tiers
 *   - Thinking about VPCs as tiers helps narrow down the context (ie app, db, general) and maximize the use of smaller network size when needed.
