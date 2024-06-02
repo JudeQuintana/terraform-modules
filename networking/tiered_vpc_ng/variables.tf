40b3358b68d1c7c0810eb428724d3c02678da269
Jude Quintana <jude.quintana@gmail.com>
2024-06-01 21:10:43 -0600

    try compact

--- networking/tiered_vpc_ng/variables.tf
+++ networking/tiered_vpc_ng/variables.tf
@@ -73,7 +73,8 @@ variable "tiered_vpc" {
   validation {
     condition = alltrue([
       for this in var.tiered_vpc.azs :
-      anytrue(this.public_subnets[*].natgw) ? length([for subnet in this.public_subnets : subnet.natgw if subnet.natgw]) == 1 : true
+      #anytrue(this.public_subnets[*].natgw) ? length([for subnet in this.public_subnets : subnet.natgw if subnet.natgw]) == 1 : true
+      anytrue(this.public_subnets[*].natgw) ? length(compact(this.public_subnets)) == 1 : true
     ])
     error_message = "There can be only be 1 public subnet with a NATGW enabled per AZ."
   }
