7718636959c6ebcd1875194cc28cbbfe316cf49b
Jude Quintana <jude.quintana@gmail.com>
2024-06-03 13:39:33 -0600

    wip wippin

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/modules/generate_routes_to_other_vpcs/base.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/modules/generate_routes_to_other_vpcs/base.tf
@@ -1,17 +1,18 @@
 # generate routes to other VPC network_cidrs, secondary_cidrs and ipv6_newtork_cidrs in private and public route tables for each VPC
 locals {
-  # { vpc-1-network_cidr => [ "vpc-1-private-rtb-id-1", "vpc-1-public-rtb-id-1", ... ], ...}
-  vpc_network_cidr_to_route_table_ids = {
-    for this in var.vpcs :
-    this.network_cidr => concat(this.private_route_table_ids, this.public_route_table_ids)
-  }
+  network_cidrs_with_route_table_ids = [
+    for this in var.vpcs : {
+      network_cidrs   = concat([this.network_cidr], this.secondary_network_cidrs, compact([this.ipv6_network_cidr]))
+      route_table_ids = concat(this.private_route_table_ids, this.public_route_table_ids)
+    }
+  ]
 
   # [ { rtb_id = "vpc-1-rtb-id-123", other_network_cidrs = [ "other-vpc-2-network_cidr", "other-vpc3-network_cidr", ... ] }, ...]
   associated_route_table_ids_with_other_network_cidrs = flatten([
-    for network_cidr, route_table_ids in local.vpc_network_cidr_to_route_table_ids : [
-      for this in route_table_ids : {
-        route_table_id      = this
-        other_network_cidrs = [for n in keys(local.vpc_network_cidr_to_route_table_ids) : n if n != network_cidr]
+    for this in local.network_cidrs_with_route_table_ids : [
+      for route_table_id in this.route_table_ids : {
+        route_table_id      = route_table_id
+        other_network_cidrs = [for n in local.vpc_cidrs_with_route_table_ids[*].network_cidrs : n if !contains(this.network_cidrs, n)]
   }]])
 
   # the better way to serve routes like hotcakes
