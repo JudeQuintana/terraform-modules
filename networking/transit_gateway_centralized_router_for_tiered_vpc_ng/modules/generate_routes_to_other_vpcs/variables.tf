0a16820e04e8d05fbe0fb14b9c1f0def212f0122
Jude Quintana <jude.quintana@gmail.com>
2024-06-03 19:46:46 -0600

    validation

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/modules/generate_routes_to_other_vpcs/variables.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/modules/generate_routes_to_other_vpcs/variables.tf
@@ -11,6 +11,16 @@ variable "vpcs" {
   # im using a manual CIDR notation check here because there are no vpc resources in use to validate the CIDR for me.
   validation {
     condition     = alltrue([for this in var.vpcs : can(cidrnetmask(this.network_cidr))])
-    error_message = "The VPC network_cidr must be in valid IPv4 CIDR notation ie 10.46.0.0/20, x.x.x.x/xx ."
+    error_message = "The VPC network_cidr must be in valid IPv4 CIDR notation ie 10.46.0.0/20, x.x.x.x/xx . Check for typos."
+  }
+
+  validation {
+    condition = alltrue(flatten([
+      for this in var.vpcs : [
+        for secondary_network_cidr in this.secondary_network_cidrs :
+        can(cidrnetmask(secondary_network_cidr))
+      ]
+    ]))
+    error_message = "Each Secondary VPC network CIDR valid IPv4 CIDR notation (ie x.x.x.x/xx -> 10.46.0.0/20). Check for typos."
   }
 }
