70475fba5d9e6f2a75494cad271bcd37302a4588
Jude Quintana <jude.quintana@gmail.com>
2024-07-10 22:22:08 -0600

    clean up and reorg

--- networking/tiered_vpc_ng/private.tf
+++ networking/tiered_vpc_ng/private.tf
@@ -24,10 +24,9 @@ locals {
 
   #ipv6 dual stack
   private_ipv6_subnet_cidrs               = toset(flatten([for this in var.tiered_vpc.azs : compact(this.private_subnets[*].ipv6_cidr)]))
-  private_ipv6_az_to_eigw                 = { for az, this in var.tiered_vpc.azs : az => this.eigw if this.eigw }
   private_ipv6_any_eigw_enabled           = length(local.private_ipv6_az_to_eigw) > 0
   private_subnet_cidr_to_ipv6_subnet_cidr = merge([for this in var.tiered_vpc.azs : zipmap(this.private_subnets[*].cidr, this.private_subnets[*].ipv6_cidr)]...)
-  private_ipv6_subnet_cidr_to_subnet_cidr = merge([for this in var.tiered_vpc.azs : { for subnet in this.private_subnets : subnet.ipv6_cidr => subnet.cidr if subnet.ipv6_cidr != null && this.eigw }]...)
+  private_ipv6_az_to_eigw                 = { for az, this in var.tiered_vpc.azs : az => this.eigw if this.eigw }
 }
 
 resource "aws_subnet" "this_private" {
