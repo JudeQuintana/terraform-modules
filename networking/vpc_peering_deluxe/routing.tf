5201ffd5f99244c352d183bffe2760ff3ea53208
Jude Quintana <jude.quintana@gmail.com>
2024-09-21 21:28:47 -0600

    fixes

--- networking/vpc_peering_deluxe/routing.tf
+++ networking/vpc_peering_deluxe/routing.tf
@@ -1,11 +1,12 @@
 locals {
   route_format = "%s|%s"
 
-  only_route_subnet_cidrs = length(var.vpc_peering_deluxe.local.only_route_subnet_cidrs) > 0 && length(var.vpc_peering_deluxe.peer.only_route_subnet_cidrs) > 0
+  only_route_subnet_cidrs      = length(var.vpc_peering_deluxe.local.only_route_subnet_cidrs) > 0 && length(var.vpc_peering_deluxe.peer.only_route_subnet_cidrs) > 0
+  only_route_ipv6_subnet_cidrs = length(var.vpc_peering_deluxe.local.only_route_ipv6_subnet_cidrs) > 0 && length(var.vpc_peering_deluxe.peer.only_route_ipv6_subnet_cidrs) > 0
 
   local_vpc_route_table_ids   = toset(concat(var.vpc_peering_deluxe.local.vpc.private_route_table_ids, var.vpc_peering_deluxe.local.vpc.public_route_table_ids))
   local_vpc_subnet_cidrs      = local.only_route_subnet_cidrs ? toset(var.vpc_peering_deluxe.local.only_route_subnet_cidrs) : toset(concat(var.vpc_peering_deluxe.local.vpc.private_subnet_cidrs, var.vpc_peering_deluxe.local.vpc.public_subnet_cidrs))
-  local_vpc_ipv6_subnet_cidrs = local.only_route_ipv6_subnet_cidrs ? toset(var.vpc_peering_deluxe.local.only_route_subnet_cidrs) : toset(concat(var.vpc_peering_deluxe.local.vpc.private_ipv6_subnet_cidrs, var.vpc_peering_deluxe.local.vpc.public_ipv6_subnet_cidrs)) # dont need compact()
+  local_vpc_ipv6_subnet_cidrs = local.only_route_ipv6_subnet_cidrs ? toset(var.vpc_peering_deluxe.local.only_route_ipv6_subnet_cidrs) : toset(concat(var.vpc_peering_deluxe.local.vpc.private_ipv6_subnet_cidrs, var.vpc_peering_deluxe.local.vpc.public_ipv6_subnet_cidrs)) # dont need compact()
 
   peer_route_table_ids       = toset(concat(var.vpc_peering_deluxe.peer.vpc.private_route_table_ids, var.vpc_peering_deluxe.peer.vpc.public_route_table_ids))
   peer_vpc_subnet_cidrs      = local.only_route_subnet_cidrs ? toset(var.vpc_peering_deluxe.peer.only_route_subnet_cidrs) : toset(concat(var.vpc_peering_deluxe.peer.vpc.private_subnet_cidrs, var.vpc_peering_deluxe.peer.vpc.public_subnet_cidrs))
@@ -24,12 +25,12 @@ locals {
 
   local_vpc_ipv6_routes_to_peer_vpc_ipv6_subnet_cidrs = [
     for local_vpc_route_table_id_and_peer_vpc_ipv6_subnet_cidr in setproduct(local.local_vpc_route_table_ids, local.peer_vpc_ipv6_subnet_cidrs) : {
-      route_table_id         = local_vpc_route_table_id_and_peer_vpc_ipv6_subnet_cidr[0]
-      destination_cidr_block = local_vpc_route_table_id_and_peer_vpc_ipv6_subnet_cidr[1]
+      route_table_id              = local_vpc_route_table_id_and_peer_vpc_ipv6_subnet_cidr[0]
+      destination_ipv6_cidr_block = local_vpc_route_table_id_and_peer_vpc_ipv6_subnet_cidr[1]
   }]
 
   local_new_vpc_ipv6_routes_to_peer_vpc_ipv6_subnet_cidrs = {
-    for this in local.local_vpc_routes_to_peer_vpc_subnet_cidrs :
+    for this in local.local_vpc_ipv6_routes_to_peer_vpc_ipv6_subnet_cidrs :
     format(local.route_format, this.route_table_id, this.destination_ipv6_cidr_block) => this
   }
 
