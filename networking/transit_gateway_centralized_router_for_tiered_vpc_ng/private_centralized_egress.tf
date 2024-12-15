bb94d5abe47d8353848dd4c1c1151e92facd1f58
Jude Quintana <jude.quintana@gmail.com>
2024-12-15 15:46:39 -0700

    update

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/private_centralized_egress.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/private_centralized_egress.tf
@@ -1,23 +1,23 @@
 locals {
-  route_any_cidr                             = "0.0.0.0/0"
-  private_centralized_egress_route_table_ids = toset(flatten([for this in var.centralized_router.vpcs : this.private_route_table_ids if this.private_centralized_egress.opt_in]))
+  private_ipv4_centralized_egress_route_any_cidr  = "0.0.0.0/0"
+  private_ipv4_centralized_egress_route_table_ids = { for this in var.centralized_router.vpcs : this.private_route_table_ids => local.private_ipv4_centralized_egress_route_any_cidr if this.private_centralized_egress.opt_in }
 }
 
 resource "aws_route" "this_private_centralized_egress_vpc_route_out" {
-  for_each = local.private_centralized_egress_route_table_ids
+  for_each = local.private_ipv4_centralized_egress_route_table_id_to_route_any_cidr
 
-  destination_cidr_block = local.route_any_cidr
+  destination_cidr_block = each.value
   route_table_id         = each.key
   transit_gateway_id     = aws_ec2_transit_gateway.this.id
 }
 
 locals {
   # validation: should only be one
-  private_centralized_egress_route_any_cidr_to_central_vpc_id = { for this in var.centralized_router.vpcs : local.route_any_cidr => this.id if this.private_centralized_egress.central }
+  private_ipv4_centralized_egress_route_any_cidr_to_central_vpc_id = { for this in var.centralized_router.vpcs : local.private_ipv4_centralized_egress_route_any_cidr => this.id if this.private_centralized_egress.central }
 }
 
-resource "aws_ec2_transit_gateway_route" "this_centralized_egress_tgw_central_vpc_routes" {
-  for_each = local.private_centralized_egress_route_any_cidr_to_central_vpc_id
+resource "aws_ec2_transit_gateway_route" "this_private_centralized_egress_tgw_central_vpc_routes" {
+  for_each = local.private_ipv4_centralized_egress_route_any_cidr_to_central_vpc_id
 
   destination_cidr_block         = each.key
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
