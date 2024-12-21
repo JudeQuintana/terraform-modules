2ffd34da413d4bdfc423378413b72aa219197bfa
Jude Quintana <jude.quintana@gmail.com>
2024-12-21 15:25:30 -0700

    comments

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/vpc_routes.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/vpc_routes.tf
@@ -1,5 +1,6 @@
 locals {
-  # the if statement controls wether or not to add the vpc and it's azs the to the mesh
+  # the if will add the vpc and it's azs the to the mesh if there's 1 or more AZs with spcecial = true
+  # if there are no AZs with special = true then the AZ is fully removed from the mesh
   vpcs = {
     for this in var.centralized_router.vpcs :
     this.id => this
