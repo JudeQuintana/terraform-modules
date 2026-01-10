a62c512d9979fa5982215f6d8b6aa58a83cbba01
Jude Quintana <jude.quintana@gmail.com>
2026-01-10 15:55:04 -0700

    reorg base

---
+++ networking/tgw_super_router_for_tgw_centralized_router/peer_base.tf
@@ -0,0 +1,53 @@
+data "aws_caller_identity" "this_peer" {
+  provider = aws.peer
+}
+
+data "aws_region" "this_peer" {
+  provider = aws.peer
+}
+
+locals {
+  peer_account_id           = data.aws_caller_identity.this_peer.account_id
+  peer_region_name          = data.aws_region.this_peer.name
+  peer_region_label         = lookup(var.region_az_labels, local.peer_region_name)
+  peer_super_router_name    = format("%s-%s-%s", local.super_router_name, var.super_router.name, local.peer_region_label)
+  peering_name_format       = "%s <-> %s"
+  peering_super_router_name = format(local.peering_name_format, local.local_super_router_name, local.peer_super_router_name)
+}
+
+resource "aws_ec2_transit_gateway" "this_peer" {
+  provider = aws.peer
+
+  amazon_side_asn                 = var.super_router.peer.amazon_side_asn
+  default_route_table_association = "disable"
+  default_route_table_propagation = "disable"
+  tags = merge(
+    local.default_tags,
+    { Name = local.peer_super_router_name }
+  )
+
+  lifecycle {
+    # cant use dynamic block for lifecycle blocks
+    # preconditions are evaluated only on apply
+    # see preconditions.tf
+    precondition {
+      condition     = local.local_provider_to_local_tgws_region_check.condition
+      error_message = local.local_provider_to_local_tgws_region_check.error_message
+    }
+
+    precondition {
+      condition     = local.local_provider_to_local_tgws_account_id_check.condition
+      error_message = local.local_provider_to_local_tgws_account_id_check.error_message
+    }
+
+    precondition {
+      condition     = local.peer_provider_to_peer_tgws_region_check.condition
+      error_message = local.peer_provider_to_peer_tgws_region_check.error_message
+    }
+
+    precondition {
+      condition     = local.peer_provider_to_peer_tgws_account_id_check.condition
+      error_message = local.peer_provider_to_peer_tgws_account_id_check.error_message
+    }
+  }
+}
