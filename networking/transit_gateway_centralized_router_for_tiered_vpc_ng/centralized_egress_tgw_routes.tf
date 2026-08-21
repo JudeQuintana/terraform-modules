8308d237db2c613ca4d3a64844840f7662a0831c
Jude Quintana <jude.quintana@gmail.com>
2026-08-20 18:17:07 -0600

    fixes

--- networking/transit_gateway_centralized_router_for_tiered_vpc_ng/centralized_egress_tgw_routes.tf
+++ networking/transit_gateway_centralized_router_for_tiered_vpc_ng/centralized_egress_tgw_routes.tf
@@ -1,16 +1,16 @@
 locals {
   centralized_egress_route_any_cidr = "0.0.0.0/0"
   # will only be a map of 1 via validation if enabled
-  centralized_egress_central_vpc_id_to_route_any_cidr = {
+  centralized_egress_central_vpc_name_to_route_any_cidr = {
     for this in local.vpcs :
-    this.id => local.centralized_egress_route_any_cidr
+    this.name => local.centralized_egress_route_any_cidr
     if this.centralized_egress_central && !contains(var.centralized_router.blackhole.cidrs, local.centralized_egress_route_any_cidr)
   }
 }
 
 # central tgw route
 resource "aws_ec2_transit_gateway_route" "this_centralized_egress_tgw_central_vpc_route_any" {
-  for_each = local.centralized_egress_central_vpc_id_to_route_any_cidr
+  for_each = local.centralized_egress_central_vpc_name_to_route_any_cidr
 
   destination_cidr_block         = each.value
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.this.id
